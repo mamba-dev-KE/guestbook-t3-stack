3817d09ff6e058484de1341a9a96eda2f4c64ae1
Joseph Maramba <joseph@smartmile.eu>
2022-08-13 12:08:41 +0300

    add in form

---
+++ src/components/Form.tsx
@@ -0,0 +1,30 @@
+interface FormProps {
+	message: string;
+	handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+	handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+export const Form: React.FC<FormProps> = ({
+	message,
+	handleChange,
+	handleSubmit,
+}) => {
+	return (
+		<form className="flex gap-2" onSubmit={handleSubmit}>
+			<input
+				type="text"
+				value={message}
+				placeholder="Your message..."
+				maxLength={100}
+				onChange={handleChange}
+				className="px-4 py-2 rounded-md border-2 border-zinc-800 bg-neutral-900 focus:outline-none"
+			/>
+			<button
+				type="submit"
+				className="p-2 rounded-md border-2 border-zinc-800 focus:outline-none"
+			>
+				Submit
+			</button>
+		</form>
+	);
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,92 @@
-import type { NextPage } from "next";
-import { signIn, signOut, useSession } from "next-auth/react";
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { trpc } from '../utils/trpc';
+import type { NextPage } from 'next';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { Form } from '../components/Form';
 
 const Home: NextPage = () => {
+	const [message, setMessage] = useState('');
 	const { data: session, status } = useSession();
+	const { mutate } = trpc.useMutation('guestbook.postMessage');
 
-	if (status === "loading") {
-		return <div>Loading...</div>;
+	if (status === 'loading') {
+		return <main className="flex flex-col items-center pt-4">Loading...</main>;
+	}
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+
+		mutate({
+			name: session?.user?.name as string,
+			message,
+		});
+
+		setMessage('');
+	};
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) =>
+		setMessage(event.target.value);
+
+	return (
+		<main className="flex flex-col items-center">
+			<h1 className="text-3xl pt-4">Guestbook</h1>
+			<div className="pt-10">
+				{session ? (
+					<div>
+						<p>hi {session.user?.name}</p>
+						<button onClick={() => signOut()}>Logout</button>
+
+						<div className="pt-6">
+							<Form
+								handleSubmit={handleSubmit}
+								message={message}
+								handleChange={handleChange}
+							/>
+						</div>
+
+						<div className="pt-10">
+							<Messages />
+						</div>
+					</div>
+				) : (
+					<div className="">
+						<button onClick={() => signIn('discord')}>
+							Login with Discord
+						</button>
+						<div className="pt-10">
+							<Messages />
+						</div>
+					</div>
+				)}
+			</div>
+
+			{process.env.NODE_ENV !== 'production' && (
+				<ReactQueryDevtools initialIsOpen={false} />
+			)}
+		</main>
+	);
+};
+
+const Messages = () => {
+	const { data: messages, isLoading } = trpc.useQuery([
+		'guestbook.getAllMessages',
+	]);
+	console.log(messages);
+
+	if (isLoading) {
+		return <div>Fetching messages...</div>;
 	}
 
 	return (
-		<main>
-			<h1>Guestbook</h1>
-			{session ? (
-				<div className="">
-					<p>hi {session.user?.name}</p>
-					<button onClick={() => signOut()}>Logout</button>
+		<div className="flex flex-col gap-4">
+			{messages?.map((msg, index) => (
+				<div key={index}>
+					<p>{msg.message}</p>
+					<span>- {msg.name}</span>
 				</div>
-			) : (
-				<div className="">
-					<button onClick={() => signIn("discord")}>Login with Discord</button>
-				</div>
-			)}
-		</main>
+			))}
+		</div>
 	);
 };
 
